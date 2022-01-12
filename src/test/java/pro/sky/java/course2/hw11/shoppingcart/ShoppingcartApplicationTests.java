272dd3dadbf23636b05724f580aab8256232e60e
Валентина <valentinka-2892@yandex.ru>
2022-01-12 20:26:43 +0500

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/hw11/shoppingcart/ShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw11.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
